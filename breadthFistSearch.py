fef5f260e5e47fe5681518cacc5e604d8e782aa5
Kayo Renato <kayorenatocontato@gmail.com>
2020-12-16 23:20:21 -0300

    Feature: Added second option of BFS

--- breadthFistSearch.py
+++ breadthFistSearch.py
@@ -39,6 +39,8 @@ class Grafo:
                     queue.append(i)
                     visited[i] = True
 
+        print()
+
 
 # criando uam instancia da classe, construindo um grafo com diversas arestas
 g = Grafo()
@@ -51,3 +53,37 @@ g.addEdge(3, 3)
 
 print('Aqui esta o caminho a seguir para atravessar o grado(comecando do vertice 2):')
 g.bfs(2)
+
+
+print("-"*40)
+
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
+
+
+print(list(bfs_paths(graph, 'A', 'F')))
+
+
+def shortest_path(graph, start, goal):
+    try:
+        return next(bfs_paths(graph, start, goal))
+    except StopIteration:
+        return None
+
+
+print(shortest_path(graph, 'A', 'F'))

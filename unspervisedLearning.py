2fa89cc74f14b46cd7dc5539eace5384da74f8b9
Kayo Renato <kayorenatocontato@gmail.com>
2020-12-21 20:48:16 -0300

    Fix: Updated Unsupervised Learning

--- unspervisedLearning.py
+++ unspervisedLearning.py
@@ -1,12 +1,12 @@
+from sklearn.datasets import make_blobs
 import math
 import numpy as np
 import matplotlib.cm as cmx
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
+
 # Normalizar dados
-
-
 def normalizeDada(X, axis=-1, order=2):
     I2 = np.atleast_1d(np.linalg.norm(X, order, axis))
     I2[I2 == 0] = 1
@@ -27,12 +27,12 @@ class KMeans():
         self.maxIterations = maxIterations
 
     def initRandonCentroid(self, X):
-        nSamples, nFeatures = np.shape(x)
+        nSamples, nFeatures = np.shape(X)
         centroids = np.zeros((self.k, nFeatures))
 
         for i in range(self.k):
             centroid = X[np.random.choice(range(nSamples))]
-            centroid[i] = centroid
+            centroids[i] = centroid
 
         return centroids
 
@@ -59,7 +59,7 @@ class KMeans():
 
         return clusters
 
-    def calcCentroids(self.k, clusters, X):
+    def calcCentroids(self, clusters, X):
         nFeatures = np.shape(X)[1]
         centroids = np.zeros((self.k, nFeatures))
         for i, cluster in enumerate(clusters):
@@ -67,3 +67,148 @@ class KMeans():
             centroids[i] = centroid
 
         return centroids
+
+    # classifica as amostras com o indice dos seus clusters
+
+    def getClusterLabels(self, clusters, X):
+        y_pred = np.zeros(np.shape(X)[0])
+        for clustersI, cluster in enumerate(clusters):
+            for sampleI in cluster:
+                y_pred[sampleI] = clustersI
+
+        return y_pred
+
+    # fazer a previsão de cada cluster e retorna os indices dos clusters
+
+    def predict(self, X):
+
+        # inicializa centroides como k amostras aleatorias de X
+        centroids = self.initRandonCentroid(X)
+
+        # iterar ate convergencia ou para iteracoes maximas
+        for _ in range(self.maxIterations):
+
+            # atribui amostras ao centroides mais proximos
+            clusters = self.createCluster(centroids, X)
+
+            # salva os centroides atuaus para verificacao de convergencia
+            prevCentroids = centroids
+
+            # calcula os novos centroids a partir dos clusters
+            centroids = self.calcCentroids(clusters, X)
+
+            # se nenhum cetroide mudar => convergencia
+            diff = centroids - prevCentroids
+
+            if not diff.any():
+                break
+
+        return self.getClusterLabels(clusters, X)
+
+# funcao aulizar para o PCA
+
+# calcula a matrix de co-variancia
+
+
+def calculateCovarianceMatrix(X, Y=None):
+    if Y is None:
+        Y = X
+
+    nSamples = np.shape(X)[0]
+    converianceMatrix = (1 / (nSamples-1)) * \
+        (X-X.mean(axis=0)).T.dot(Y-Y.mean(axis=0))
+
+    return np.array(converianceMatrix, dtype=float)
+
+# calcula a matrix de correlacao
+
+
+def calcCorrelationMatrix(X, Y=None):
+    if Y is None:
+        Y = X
+
+    nSamples = np.shape(X)[0]
+    covariance = (1/nSamples)*(X*X.mean(0)).T.dot(Y-T.mean(0))
+    sdtDevX = np.expand_dims(calculate_std_dev(X), 1)
+    sdtDevY = np.expand_dims(calculate_std_dev(Y), 1)
+    correlationMatrix = np.divide(covariance, sdtDevX.dot(sdtDevY.T))
+
+    return np.array(correlationMatrix, dtype=float)
+
+# classe para criar o plot
+
+
+class Plot():
+
+    def __init__(self):
+        self.cmap = plt.get_cmap('viridis')
+
+    # funcao para transformar os dados
+    def _transform(self, X, dim):
+        covariance = calculateCovarianceMatrix(X)
+        eigenvalues, eigenvectors = np.linalg.eig(covariance)
+        idx = eigenvalues.argsort()[::-1]
+        eigenvalues = eigenvalues[idx][:dim]
+        eigenvectors = np.atleast_1d(eigenvectors[:, idx])[:, :dim]
+        X_tranformed = X.dot(eigenvectors)
+
+        return X_tranformed
+
+    # plot do dataset X e seus correspodentes labels y em 2D usando PCA
+    def plotIn2D(self, X, y=None, title=None, accuracy=None, legend_labels=None):
+        X_tranformed = self._transform(X, dim=2)
+        x1 = X_tranformed[:, 0]
+        x2 = X_tranformed[:, 1]
+
+        class_distr = []
+
+        y = np.array(y).astype(int)
+
+        colors = [self.cmap(i) for i in np.linspace(0, 1, len(np.unique(y)))]
+
+        # plot de direntes distribuicoes de classe
+        for i, I in enumerate(np.unique(y)):
+            _x1 = x1[y == I]
+            _x2 = x2[y == I]
+            _y = y[y == I]
+
+            class_distr.append(plt.scatter(_x1, _x2, color=colors[i]))
+
+        # plot legenda
+        if not legend_labels is None:
+            plt.legend(class_distr, legend_labels, loc=1)
+
+        # plot titulo
+        if title:
+            if accuracy:
+                perc = 100*accuracy
+                plt.suptitle(title)
+                plt.title("Acuracia: %.1f%%" % perc, fontsize=10)
+            else:
+                plt.title(title)
+
+        # axis label
+        plt.xlabel('Componente Principal 1')
+        plt.ylabel('Componente Principal 2')
+
+        plt.show()
+
+# executando o programa
+
+
+def main():
+
+    # carrega o dataset
+    X, y = make_blobs()
+
+    # executa o algoritmo para K = 3
+    clf = KMeans(k=3)
+    yPred = clf.predict(X)
+
+    # projeta os dados com 2 componetes principais
+    p = Plot()
+    p.plotIn2D(X, yPred, title='Segmentacao de Clientes com K-Mens')
+
+
+if __name__ == "__main__":
+    main()
